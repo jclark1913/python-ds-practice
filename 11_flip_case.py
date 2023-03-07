9c239abb0ad5d85eae0825e35b13bc382250f750
Kevin Nguyen <kayvinhnguyen@gmail.com>
2023-03-06 17:31:56 -0800

    finished 11

--- 11_flip_case.py
+++ 11_flip_case.py
@@ -11,3 +11,23 @@ def flip_case(phrase, to_swap):
         'AaaaHHH'
 
     """
+    # .upper() converts
+    # check if to_swap is lower or upper
+    # if lower
+        # convert all uppers in phrase to lowercase
+    # if to_swap is upper
+        # convert all lowercase in phrase to uppercase
+
+    flipped_str = ""
+
+    for letter in phrase:
+        if letter.lower() == to_swap.lower():
+            if letter.islower():
+                flipped_str += letter.upper()
+            elif letter.isupper():
+                flipped_str += letter.lower()
+        else:
+            flipped_str += letter
+            
+    return flipped_str
+            
