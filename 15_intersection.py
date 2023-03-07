d53d2f25b930ae4eacdca7348decf926ec3fe2fa
Justin Clark <jclarksummit@gmail.com>
2023-03-07 15:25:56 -0500

    Finished 10-15

--- 15_intersection.py
+++ 15_intersection.py
@@ -1,15 +1,24 @@
 def intersection(l1, l2):
     """Return intersection of two lists as a new list::
-    
+
         >>> intersection([1, 2, 3], [2, 3, 4])
         [2, 3]
-        
+
         >>> intersection([1, 2, 3], [1, 2, 3, 4])
         [1, 2, 3]
-        
+
         >>> intersection([1, 2, 3], [3, 4])
         [3]
-        
+
         >>> intersection([1, 2, 3], [4, 5, 6])
         []
     """
+    # intersections = []
+
+    # for item in l1:
+    #     if item in l2:
+    #         intersections.append(item)
+
+    # return intersections
+
+    return [intersection for intersection in l1 if intersection in l2]
